6bc2b504d14c4a92445bc2c8c7418a67bb30dc68
Samin Israr Ravi <saminisrar1@gmail.com>
2022-03-25 22:28:31 +0600

    visit section added

--- src/components/Visit/Visit.js
+++ src/components/Visit/Visit.js
@@ -3,8 +3,17 @@ import './Visit.css';
 
 const Visit = () => {
     return (
-        <div>
-                <h1 className="text-center">Visiting List</h1>
+        <div >
+            <div className="mt-4 visit-card">
+
+                <h2 className="text-center pt-4">Select Countries</h2>
+
+                <div className="d-flex flex-column align-items-center btn-div mx-5">
+                    <button className="btn btn-primary px-5 mb-4">Choose 1 Country To Visit</button>
+                    <button className="btn btn-danger px-5">Choose Again</button>
+                </div>
+
+            </div>
             
         </div>
     );
