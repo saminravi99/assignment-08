8c40a2b1ac178030c53e24a0a8d39973617b8d58
Samin Israr Ravi <saminisrar1@gmail.com>
2022-03-26 01:55:33 +0600

    error handled

--- src/components/Visit/Visit.js
+++ src/components/Visit/Visit.js
@@ -29,23 +29,24 @@ const Visit = (props) => {
 
                 <h2 className="text-center pt-4">Select Countries</h2>
                 <div className="d-flex flex-column align-items-center">
-                    {random === -1 ? visitingCountries : 
+                    {random === -1 ? visitingCountries : getRandomCountry ? 
                     <div className="row mt-4 w-100 align-items-center">
                         <div className="col-4 d-flex justify-content-end ">
-                            <img className="img-fluid visit-img " src={getRandomCountry.picture} alt="" />
+                            <img className="img-fluid visit-img " src={getRandomCountry?.picture} alt="" />
                         </div>
                         <div className="col-4 text-center">
-                            <h6 >{getRandomCountry.country}</h6>
+                            <h6 >{getRandomCountry?.country}</h6>
                         </div>
                         <div className="col-4 ">
                             <i class="fa-solid fa-plane-departure"></i>
                         </div>
-                    </div>
+                    </div> : null
                     }
+                    
                 </div>
                 <div className="d-flex flex-column align-items-center btn-div ms-5">
-                    <button onClick={chooseRandom} className="btn btn-primary px-5 py-2 mb-4">Vsit A Country <i class="fa-solid fa-earth-americas ms-2 "></i> </button>
-                    <button onClick={clearList}className="btn btn-danger py-2 px-5">Choose Again <i class="fa-solid fa-clipboard-list ms-2"></i></button>
+                    <button onClick={chooseRandom} className="btn btn-primary px-5 py-2 mb-4">Vsit A Country <i className="fa-solid fa-earth-americas ms-2 "></i> </button>
+                    <button onClick={clearList}className="btn btn-danger py-2 px-5">Choose Again <i className="fa-solid fa-clipboard-list ms-2"></i></button>
                 </div>
 
             </div>
