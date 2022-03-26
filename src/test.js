3bd93bd3790e2cc379ef3f94793945f506b6934d
Samin Israr Ravi <saminisrar1@gmail.com>
2022-03-26 13:16:35 +0600

    code cleaned

--- src/test.js
+++
@@ -1,8 +0,0 @@
-
-
-                <div>
-                    {getRandomObject !== -1 ? <h1>{getRandomObject?.name}</h1> : 
-                      <Card.Title>Selected Perfumes: {cart.length}</Card.Title>
-                      <Card.Text><ul > {cart.map(product => <li key={product.id}>{product.name}</li>)}  </ul></Card.Text>
-                     }
-                </div>
